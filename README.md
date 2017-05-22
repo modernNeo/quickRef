a5ea6168b6c9ea7418b20ef44516857c17b6b2c7
modernNeo <jason@jason_arch.telus>
2017-05-22 01:05:39 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+this is a quick reference script to remind myself of some commands i regularly use to save myself the time of opening up man commands
---
+++ commands.sh
@@ -0,0 +1,37 @@
+#~/bin/bash
+
+echo -e "listing files [1]\t\tgit [2]\t\tAUR install [3\t\tmount external [4]\t\tmount MacBook [5]"
+
+read option
+if [ $option -eq 1 ]; then
+	echo "du -sh *"
+elif [ $option -eq 2 ]; then
+	echo "save local changes and push them to server"
+	echo "git add -A"
+	echo "gi commit -m "what are the lastest changes""
+	echo "git push"
+	echo " "
+	echo "create copy of current branch and call it feature1"
+	echo "git branch feature1"
+	echo " "
+	echo "switch to branch feature1"
+	echo "git checkout feature1"
+	echo " "
+	echo "More Info: http://tutorialzine.com/2016/06/learn-git-in-30-minutes/"
+elif [ $option -eq 3 ]; then
+	echo "Download snapshot link"
+	echo "wget https://aur.archlinux.org/cgit/aur.git/snapshot/dropbox.tar.gz"
+	echo "tar xvf dropbox.tar.gz"
+	echo "makepkg"
+	echo "sudo pacman -U dropbox-23.4.18-1-x86_64.pkg.tar.xz"
+	echo "https://www.youtube.com/watch?v=xXpO6jsbrF0&t=611s"
+elif [ $option -eq 4 ]; then
+	echo "lsblk -f"
+	echo "sudo mount UUID=5b09bc13-41e4-350f-b021-e1c5e59940fb /mnt/jason-hard_drive/"
+elif [ $option -eq 5 ]; then
+	echo "sudo mount -t cifs //192.168.1.64/Macintosh\ HD /mnt/jason-macbook -o user="Jason Manshad""
+fi
+
+
+
+
