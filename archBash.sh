ffc3ce3a8081ca0656cc48a0dacd3f5681a23991
modernNeo <j_manshad@sfu.ca>
2017-10-07 13:19:12 -0700

    removed path for grun as its no longer needed

--- archBash.sh
+++ archBash.sh
@@ -11,9 +11,4 @@ alias ...='cd ../..'
 alias vi='vim'
 dir=`pwd`
 alias commands='$dir/commands.sh'
-###ONLY FOR ANKLR FOR CMPT 397
-export CLASSPATH=".:/usr/local/lib/antlr-4.5.3-complete.jar:$CLASSPATH"
-alias antlr4='java -Xmx500M -cp "/usr/local/lib/antlr-4.5.3-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
-alias grun='java org.antlr.v4.gui.TestRig'
-###
 cd ~/
--- macBash.sh
+++ macBash.sh
@@ -13,8 +13,3 @@ cd GitHub/quickRef
 dir=`pwd`
 alias commands='$dir/commands.sh'
 cd ~/
-###ONLY FOR ANKLR FOR CMPT 397
-export CLASSPATH=".:/usr/local/lib/antlr-4.5.3-complete.jar:$CLASSPATH"
-alias antlr4='java -Xmx500M -cp "/usr/local/lib/antlr-4.5.3-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
-alias grun='java org.antlr.v4.gui.TestRig'
-###
