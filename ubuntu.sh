62271dc81160f33adb1a9d7cc683d5bf4bdbadd3
modernNeo <j_manshad@sfu.ca>
2017-10-16 10:26:32 -0700

    adding ubuntu version

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-
+cd ~/GitHub/quickRef/
 
 PS1="\d \A \u:\h\$(pwd) $"
-alias ll='ls -l'
+#alias ll='ls -l'
 alias la='ls -la'
 alias l='ls -alF'
 
 alias ..='cd ..'
 alias ...='cd ../..'
 alias vi='vim'
-
-cd GitHub/quickRef
 dir=`pwd`
 alias commands='$dir/commands.sh'
+
 cd ~/
