f3351f89ec1be919e8d365b62c5403c8c042ca15
Jace Manshadi <j_manshad@sfu.ca>
2017-12-30 18:30:28 -0800

    fixed issue that occurs when a directory name has a space in it

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,12 +2,12 @@ PS1="\d \A \u:\h\$(pwd) $"
 #alias ll='ls -l'
 alias la='ls -la'
 alias l='ls -alF'
-
+alias ll="ls -l"
 alias ..='cd ..'
 alias ...='cd ../..'
 alias vi='vim'
 
 cd ~/GitHub/quickRef/
 dir=`pwd`
-alias commands='$dir/commands.sh'
+alias commands='"$dir/commands.sh"'
 cd ~/
