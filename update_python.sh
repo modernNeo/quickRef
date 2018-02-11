027c510f6ca922ca7e32686ab77d6dc5bd67ab62
modernNeo <j_manshad@sfu.ca>
2018-02-11 15:26:13 -0800

    added a script to update python to python3.6 on the Ubuntu 14.04, 16.04 and 17.10

--- update_python.sh
+++ update_python.sh
@@ -1,32 +1,79 @@
 #!/bin/bash -e
 set -e
+##########################################
+## REFERENCE PAGES                      ##
+## https://askubuntu.com/a/865569       ##
+## https://stackoverflow.com/a/44254088 ##
+##########################################
+create_box (){
+	output="$@"
+	greatestLength=0
+	for i in "${output[@]}"
+	do
+		if [ $greatestLength -lt ${#i} ]; then
+			greatestLength=${#i}
+		fi
+	done
 
-################################################################################################
-## TO INSTALL PYYTHON3.6 ON UBUNTU 14.04 OR 16.04 FOLLOW THE STEPS ON THIS STACKOVERFLOW PAGE ##
-## https://askubuntu.com/a/865569                                                             ##
-################################################################################################
+	# section for printing top border
+	echo; for i in $(seq $(($greatestLength + 8))); do echo -n "#"; done; echo
+	echo -n "###"; for i in $(seq $(($greatestLength + 2))); do echo -n " "; done; echo "###"
 
-## THIS SECTION ENSURES THE CORRECT PYTHON3 VERSION [3.6.3] AND PIP [9.0.1 from /usr/local/lib/python3.6/dist-packages (python 3.6)] ARE INSTALLED
-# extracted from this asnwer on stackoverflow https://stackoverflow.com/a/44254088
-sudo add-apt-repository ppa:jonathonf/python-3.6 -y
-sudo apt-get -y update
-sudo apt-get install -y python3.6 python3.6-dev python3.6-venv
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python3.6 get-pip.py
+	# sectiom for printing logs
+	for i in "${output[@]}"
+	do
+		echo -tn "### "$i
+		for i in $(seq $(($greatestLength - ${#i} +1 ))); do echo -n " "; done;
+		echo "###"
+	done
+	
+	# section for printing bottom border
+	echo -n "###"; for i in $(seq $(($greatestLength + 2))); do echo -n " "; done;	echo "###"	
+	for i in $(seq $(($greatestLength + 8))); do echo -n "#"; done;
+	echo
 
-## This section is for adding in google chrome and its drivers to vagramt as wel as selenium to interact with chrome
-cd /vagrant
-sudo pip install selenium
-wget https://chromedriver.storage.googleapis.com/2.35/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-rm chromedriver_linux64.zip
-sudo mv chromedriver  /usr/bin
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo echo '# needed for installing google-chrome' >> /etc/apt/sources.list
-sudo echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list
-sudo apt-get -y update
-sudo apt-get -y install google-chrome-stable
-sudo python3.6 -m pip install -r requirements.txt
+}
 
-wget http://selenium-release.storage.googleapis.com/3.8/selenium-server-standalone-3.8.1.jar
-sudo mv selenium-server-standalone-3.8.1.jar /usr/local/bin/
+if [ -f /etc/os-release ]; then
+	. /etc/os-release
+	if [ $ID == "ubuntu"]; then
+		if [ $VERSION_ID == "14.04" ]; then
+			option[0]="This appears to be an Ubuntu 14.04..."
+			option[1]="Adding ppa:jonathonf/python-3.6 repository"
+			option[2]="With the following packages: "
+			option[3]="\tpython3.6"
+			option[4]="\tpython3.6-dev"
+			option[5]="Please note that due to a dependency issue with python-pip-whl"
+			option[6]="python3-venv and pip3 for python3.6 will not be installed"
+			create_box $output
+			sudo add-apt-repository ppa:jonathonf/python-3.6 -y
+			sudo apt-get -y update
+			sudo apt-get install -y python3.6 python3.6-dev 
+		elif [ $VERSION_ID == "16.04" ]; then
+			option[0]="This appears to be an Ubuntu 16.04..."
+			option[1]="Adding ppa:jonathonf/python-3.6 repository"
+			option[2]="With the following packages: "
+			option[3]="\tpython3.6"
+			option[4]="\tpython3.6-dev"
+			option[5]="\tpython3.6-venv"
+			option[6]="\tpython3.6-pip"
+			create_box $output
+			sudo add-apt-repository ppa:jonathonf/python-3.6 -y
+			sudo apt-get -y update
+			sudo apt-get install -y python3.6 python3.6-dev python3.6-venv
+			wget https://bootstrap.pypa.io/get-pip.py
+			sudo python3.6 get-pip.py
+		elif [ $VERSION_ID == "17.10" ]; then
+			option[0]="This appears to be an Ubuntu 17.10..."
+			option[1]="No need to add third-party repo as python3.6 is supported"
+			option[2]="Via Ubuntu repos"
+			option[3]="\tpython3.6"
+			option[4]="\tpython3.6-dev"
+			option[5]="\tpython3.6-venv"
+			option[6]="\tpython3.6-pip"
+			create_box $output
+			sudo apt-get -y update
+			sudo apt-get install -y python3 python3-venv python3-pip
+		fi
+	fi
+fi
