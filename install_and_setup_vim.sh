d9475f62f05f9f3d022ccef53da58f7a6db93bc0
modernNeo <j_manshad@sfu.ca>
2018-05-21 23:54:54 -0700

    updating the script with more goodies

--- install_and_setup_vim.sh
+++ install_and_setup_vim.sh
@@ -25,44 +25,52 @@ setup_vimrc (){
     echo "Setting up the desert colorscheme as the default colorscheme for vim"
     echo "colorscheme desert" >> ~/.vimrc
   fi
-  sudo update-alternatives --set editor /usr/bin/vim.basic
 }
 
-vimBinPath=`which vim`
-if [ ${#vimBinPath} -eq 0 ]; then
-  echo "vim is not installed, would you like to install it? [Y/n]"
-  read option
-  while [ "$option" != "Y" -a "$option" != "y" -a "$option" != "N" -a "$option" != "n" ]
-  do
-    echo "Incorrect input detected, please try again"
+install_vim(){
+  vimBinPath=`which vim`
+  if [ ${#vimBinPath} -eq 0 ]; then
     echo "vim is not installed, would you like to install it? [Y/n]"
     read option
-  done
-  if [ "$option" = "y" -o "$option" = "y" ]; then
-  ## detect OS and install
-    OS=`uname -s`
-    if [ "$OS" = "Darwin" ]; then
-      echo "this appears to be a Mac, now attempting to install VIM with brew"
-      brew install vim
-      exitCode=`echo $?`
-    elif [ "$OS"  = "Linux" ]; then
-      echo "this appears to be a Linux system, now attemptingto install VIM"
-      sudo apt-get install -y vim
-      exitCode=`echo $?`
-    fi
-    if [ $exitCode -ne 0 ]; then
-      echo "the install command did not appear to work correctly, please investigate further"
-    else
-      echo "vim successfully installed!"
-
-      #setting vim as the default editor
-      VIM=`which vim`.basic
-      update-alternatives --set editor $VIM
-      setup_vimrc
-      alias vi='vim'
-    fi
+    while [ "$option" != "Y" -a "$option" != "y" -a "$option" != "N" -a "$option" != "n" ]
+    do
+      echo "Incorrect input detected, please try again"
+      echo "vim is not installed, would you like to install it? [Y/n]"
+      read option
+    done
+    if [ "$option" = "y" -o "$option" = "y" ]; then
+    ## detect OS and install
+      OS=`uname -s`
+      if [ "$OS" = "Darwin" ]; then
+        echo "this appears to be a Mac, now attempting to install VIM with brew"
+        brew install vim
+        exitCode=`echo $?`
+      elif [ "$OS"  = "Linux" ]; then
+        echo "this appears to be a Linux system, now attemptingto install VIM"
+        sudo apt-get install -y vim
+        exitCode=`echo $?`
+      fi
+      if [ $exitCode -ne 0 ]; then
+        echo "the install command did not appear to work correctly, please investigate further"
+        exit 1
+      else
+        echo "vim successfully installed!"
+      fi
+    fi 
   fi
-else
-  setup_vimrc
-  alias vi='vim'
+}
+
+setup_default_editor(){
+  VIM=`which vim`.basic
+  update-alternatives --set editor $VIM
+}
+
+if [ `echo $UID` -ne 0 ]; then
+  echo "you are not a user with root privelages.....quiting script"
+  exit 1
 fi
+
+install_vim
+setup_vimrc
+setup_default_editor
+alias vi='vim'
