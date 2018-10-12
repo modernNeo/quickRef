9e7e54532f2f2d6fa7cb1f51b32fb79d775a20be
Jace Manshadi <j_manshad@sfu.ca>
2018-10-11 21:27:17 -0700

    removing debugging and making changed to allow the script to work within a WSL env

--- commands.sh
+++ commands.sh
@@ -6,12 +6,12 @@ create_array_of_quickref_names(){
 	index=0
 	FILE_TO_QUERY=$1
 	KEY_TO_QUERY=$2
-	output=`cat $FILE_TO_QUERY | jq ".${KEY_TO_QUERY}[${index}].name" -r`
+	output=`cat "$FILE_TO_QUERY" | jq ".${KEY_TO_QUERY}[${index}].name" -r`
 	while [ "$output" != null ]
 	do
-		quickReferenceNames[$index]=`cat $FILE_TO_QUERY | jq ".${KEY_TO_QUERY}[${index}].name" -r`
+		quickReferenceNames[$index]=`cat "$FILE_TO_QUERY" | jq ".${KEY_TO_QUERY}[${index}].name" -r`
 		index=$(($index+1))
-		output=`cat $FILE_TO_QUERY | jq ".${KEY_TO_QUERY}[${index}].name" -r`
+		output=`cat "$FILE_TO_QUERY" | jq ".${KEY_TO_QUERY}[${index}].name" -r`
 	done
 }
 
@@ -20,20 +20,20 @@ print_specified_index(){
 	FILE_TO_QUERY=$1
 	KEY_TO_QUERY=$2
 	INDEX_TO_QUERY=$3
-	echo "FILE_TO_QUERY="$FILE_TO_QUERY" KEY_TO_QUERY="$KEY_TO_QUERY" INDEX_TO_QUERY="$INDEX_TO_QUERY
-	output=`cat $FILE_TO_QUERY | jq ".${KEY_TO_QUERY}[${INDEX_TO_QUERY}].description[${innerIndex}]" -r`
+	output=`cat "$FILE_TO_QUERY" | jq ".${KEY_TO_QUERY}[${INDEX_TO_QUERY}].description[${innerIndex}]" -r`
 	while [ "$output" != null ]
 	do
-		isExec=`cat $FILE_TO_QUERY | jq ".${KEY_TO_QUERY}[${INDEX_TO_QUERY}].exec" -r`
+		isExec=`cat "$FILE_TO_QUERY" | jq ".${KEY_TO_QUERY}[${INDEX_TO_QUERY}].exec" -r`
 		if [ "$isExec" == "true" ]; then
 			eval "$output"
 			exit
 		fi
 		echo "$output"
 		innerIndex=$(($innerIndex+1))
-		output=`cat $FILE_TO_QUERY | jq ".${KEY_TO_QUERY}[${INDEX_TO_QUERY}].description[${innerIndex}]" -r`
+		output=`cat "$FILE_TO_QUERY" | jq ".${KEY_TO_QUERY}[${INDEX_TO_QUERY}].description[${innerIndex}]" -r`
 	done
 }
+
 ##takes the different options from commands.json and displays them for the user
 quickReferenceNames=''
 create_array_of_quickref_names "$COMMAND_FILE_LOCATION" "commands"
@@ -46,7 +46,6 @@ done
 
 #reads the option that the user wants and determines if it is a regular option selected or a git option that requires further work
 read option
-echo "selected option="$option
 if [ $option -ne 2 ]; then
 	print_specified_index "$COMMAND_FILE_LOCATION" "commands" $((option-1))
 	exit
